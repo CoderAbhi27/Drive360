619159de51279c62f218c75358fa706249040802
Aritra <aritrabiswas9788@gmail.com>
2023-08-08 03:52:43 +0530

    com5

--- app/src/main/java/com/example/drive360/Fragments/FuelTrackingFragment.kt
+++ app/src/main/java/com/example/drive360/Fragments/FuelTrackingFragment.kt
@@ -1,6 +1,7 @@
 package com.example.drive360.Fragments
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import com.example.drive360.Activity.FuelTrackingOrderActivity
 import com.example.sagarmiles.R
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieEntry
 
 class FuelTrackingFragment : androidx.fragment.app.Fragment(R.layout.fragment_fuel_tracking) {
+    private lateinit var pieChart: PieChart
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +33,47 @@ class FuelTrackingFragment : androidx.fragment.app.Fragment(R.layout.fragment_fu
         super.onViewCreated(view, savedInstanceState)
 
         val orderNowButton : Button = view.findViewById(R.id.orderNowButton)
+        pieChart=view.findViewById(R.id.piechart)
+
+        populatePieChart(pieChart)
 
         orderNowButton.setOnClickListener{
             val intent = Intent(activity, FuelTrackingOrderActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun populatePieChart(pieChart: PieChart) {
+        pieChart.isDrawHoleEnabled=true
+        pieChart.setHoleColor(Color.TRANSPARENT)
+        //pieChart.setUsePercentValues(true)
+        //pieChart.setEntryLabelColor(Color.BLACK)
+        //pieChart.setEntryLabelTextSize(12f)
+        pieChart.setDrawEntryLabels(false)
+        pieChart.centerText="MEMORY ANALYSIS"
+        pieChart.setCenterTextSize(16f)
+        pieChart.setCenterTextColor(Color.WHITE)
+
+        pieChart.description.isEnabled=false
+        pieChart.setExtraOffsets(5f,5f,20f,5f)
+
+        val l: Legend =pieChart.legend
+        l.verticalAlignment= Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment= Legend.LegendHorizontalAlignment.RIGHT
+        l.orientation= Legend.LegendOrientation.VERTICAL
+        l.setDrawInside(false)
+        l.textColor= Color.WHITE
+        l.isEnabled=true
+        l.textSize=12f
+        l.xOffset=0f
+        l.yOffset=0f
+        val values:ArrayList<PieEntry> = ArrayList()
+        values.add(PieEntry(0.42f,"FUEL COSTS"))
+        values.add(PieEntry(0.11f,"INSURANCE"))
+        values.add(PieEntry(0.5f,"TRAVEL EXPENSES"))
+        values.add(PieEntry(0.9f,"TRAVEL EXPENSES"))
+        values.add(PieEntry(0.5f,"TRAVEL EXPENSES"))
+
+
     }
 }
