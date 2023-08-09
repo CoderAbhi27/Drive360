224f8a9550b97a49217355446fe360022750f724
Aritra <aritrabiswas9788@gmail.com>
2023-08-09 15:32:54 +0530

    com10

--- app/src/main/java/com/example/drive360/Activity/FuelTrackingOrderActivity.kt
+++ app/src/main/java/com/example/drive360/Activity/FuelTrackingOrderActivity.kt
@@ -1,12 +1,32 @@
 package com.example.drive360.Activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.sagarmiles.R
 
 class FuelTrackingOrderActivity : AppCompatActivity() {
+    private lateinit var ftCompany: AutoCompleteTextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fuel_tracking_order)
+        ftCompany = findViewById(R.id.ftCompany)
+        val companies = arrayOf(
+            "Tata Motors",
+            "Eicher Motors",
+            "SML ISUZU Limited",
+            "Mahindra",
+            "Ashok Leyland",
+            "Force Motors",
+            "Hindustan Motors",
+            "BharatBenz",
+            "Volvo Trucks",
+            "Asia Motorworks"
+        )
+
+        val arrayAdapter=ArrayAdapter<String>(this, androidx.appcompat.R.layout.select_dialog_item_material,companies)
+        ftCompany.threshold=0
+        ftCompany.setAdapter(arrayAdapter)
     }
 }
